cb73e7671c4d422f28b6ac9e2bd99e1f4d5cea6b
hecunsheng <763888792@qq.com>
2018-05-15 10:19:02 +0800

    更新

--- src/main/java/run/star/plan/hecs/plat.java
+++ src/main/java/run/star/plan/hecs/plat.java
@@ -1,8 +1,10 @@
 package run.star.plan.hecs;
 
+import java.io.Serializable;
+
 /**
  * Created by hecs on 2018/5/14.
  * desc:
  */
-public class plat {
+public class plat implements Serializable{
 }
