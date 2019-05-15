ac30f6646a42a7e6a600a41d72f19bff1e784a8d
00856 <hecunsheng@qccr.com>
2019-05-15 14:27:35 +0800

    jdk1,8 stream test

---
+++ java-action/src/main/java/sun.star.plan/jdk18/TestStreamModel.java
@@ -0,0 +1,57 @@
+package sun.star.plan.jdk18;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * @description:
+ * @author: hecs
+ * @date: 2019-01-17 19:03
+ **/
+@Data
+public class TestStreamModel {
+    private int id;
+
+    private String name;
+
+    private int grade;
+
+    private int classes;
+
+    private double score;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TestStreamModel that = (TestStreamModel) o;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(o))
+                .append(id, that.id)
+                .append(grade, that.grade)
+                .append(classes, that.classes)
+                .append(score, that.score)
+                .append(name, that.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .appendSuper(super.hashCode())
+                .append(id)
+                .append(name)
+                .append(grade)
+                .append(classes)
+                .append(score)
+                .toHashCode();
+    }
+}
