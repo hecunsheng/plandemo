3ab55758cab82f4f035394ef04b8a882f8d29377
hecs <763888792@qq.com>
2021-03-15 15:41:25 +0800

    map test

---
+++ java-action/src/main/java/run/star/plan/map/MapBean.java
@@ -0,0 +1,15 @@
+package run.star.plan.map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author hecs
+ * @date 2021/3/15 15:23
+ */
+@Getter
+@Setter
+public class MapBean {
+    Integer id;
+    String title;
+}
